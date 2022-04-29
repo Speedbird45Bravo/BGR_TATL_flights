c6ff9b9afedeecdd81f76f6db0f30adb0d5524bb
Henry Loughlin <64449842+Speedbird45Bravo@users.noreply.github.com>
2022-04-29 18:59:21 -0400

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import os
+
+os.chdir("C:\\Users\\Henry\\Desktop\\BGR_TATL_flights\\Data")
 
 cols = ['FA','SS']
 
-ac = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\AC.csv", names=cols)
+ac = pd.read_csv("AC.csv", names=cols)
 ac_dict = dict(zip(ac['FA'],ac['SS']))
 
-al = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\AL.csv", names=cols)
+al = pd.read_csv("AL.csv", names=cols)
 al_dict = dict(zip(al['FA'],al['SS']))
 
-codes = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\ICAOIATA.csv", names=cols)
+codes = pd.read_csv("ICAOIATA.csv", names=cols)
 code_dict = dict(zip(codes['FA'],codes['SS']))
 
-apc = pd.read_csv("C:\\Users\\henry\\PycharmProjects\\BGR_TATL_flights\\data\\APC.csv")
+apc = pd.read_csv("APC.csv", names=['IATA','Country'])
 apc_dict = dict(zip(apc['IATA'],apc['Country']))
